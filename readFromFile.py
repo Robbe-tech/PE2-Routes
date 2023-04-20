eef7f2fc99acea5649a68604f90ecd6820277c4e
RaulLopez13 <vegamarcos1310@gmail.com>
2023-04-20 12:06:15 +0200

    Update readFromFile.py

--- readFromFile.py
+++ readFromFile.py
@@ -1,7 +1,8 @@
 import pandas as pd
+from pathlib import Path
 
 df = {
-    'start' : [],
+    'Start' : [],
     'neighbour' : []
 }
 
@@ -9,31 +10,26 @@ df = {
 text_file = open("N-wegen.txt", "r")
 data = text_file.readlines()
 
-for n in len(data):
-    if(n == 0):
-        1
-    elif(n == 1):
-        2
-    elif(n == (len(data) - 1)):
-        3
-    else:
+for n in range(len(data)):
+    if(n > 1 and n < (len(data) - 1)):
         if(data[n-2] == '\n'):
-            #Vorige lijn is dan N-weg
-            1
-        elif(data[n-1] == '\n'):
-            #Dit mijn N-weg
-            2
+            #Vorige lijn is dan N-weg, begin N-weg
+            df['Start'].append(data[n].strip())
+            df['neighbour'].append(data[n+1].strip())
         elif(data[n+1] == '\n'):
             #Einde N-weg
-            3
-        else:
+            df['Start'].append(data[n].strip())
+            df['neighbour'].append(data[n-1].strip())
+        elif(data[n-1] != '\n'):
+            #[ Data[n-1] = '\n' ] zijn de N-wegen
             #Midden van N-weg
-            4
-    
+            df['Start'].append(data[n].strip())
+            df['neighbour'].append(data[n+1].strip())
+            df['Start'].append(data[n].strip())
+            df['neighbour'].append(data[n-1].strip())
     
 
-    
-
-
-print(data)
+frame = pd.DataFrame(df)
+frame.to_csv("connections.csv")
+print(frame)
 text_file.close()
