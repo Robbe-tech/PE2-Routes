241c17a43ef2337bd6d5e9787afd4563e17833d3
RaulLopez13 <vegamarcos1310@gmail.com>
2023-04-27 12:16:24 +0200

    wegen weg krijgen
    werkt nog niet exact

--- readFromFile.py
+++ readFromFile.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 from pathlib import Path
 
 df = {
@@ -9,24 +10,26 @@ df = {
 
 text_file = open("N-wegen.txt", "r")
 data = text_file.readlines()
+weg = re.compile(r'\d?\d?\d')
 
-for n in range(len(data)):
-    if(n > 1 and n < (len(data) - 1)):
-        if(data[n-2] == '\n'):
-            #Vorige lijn is dan N-weg, begin N-weg
-            df['Start'].append(data[n].strip())
-            df['neighbour'].append(data[n+1].strip())
-        elif(data[n+1] == '\n'):
-            #Einde N-weg
-            df['Start'].append(data[n].strip())
-            df['neighbour'].append(data[n-1].strip())
-        elif(data[n-1] != '\n'):
-            #[ Data[n-1] = '\n' ] zijn de N-wegen
-            #Midden van N-weg
-            df['Start'].append(data[n].strip())
-            df['neighbour'].append(data[n+1].strip())
-            df['Start'].append(data[n].strip())
-            df['neighbour'].append(data[n-1].strip())
+print(data)
+
+for n in range(1, len(data) - 1):
+    if(data[n-2] == '\n'):
+        #Vorige lijn is dan weg, begin weg
+        df['Start'].append(data[n].strip())
+        df['neighbour'].append(data[n+1].strip())
+    elif(data[n+1] == '\n'):
+        #Einde N-weg
+        df['Start'].append(data[n].strip())
+        df['neighbour'].append(data[n-1].strip())
+    elif(data[n-1] != '\n' and weg.search(data[n-1]) == None):
+        #[ Data[n-1] = '\n' ] zijn de wegen
+        #Midden van de weg
+        df['Start'].append(data[n].strip())
+        df['neighbour'].append(data[n+1].strip())
+        df['Start'].append(data[n].strip())
+        df['neighbour'].append(data[n-1].strip())
     
 
 frame = pd.DataFrame(df)
